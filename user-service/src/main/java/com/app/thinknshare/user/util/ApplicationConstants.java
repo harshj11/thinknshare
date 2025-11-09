521f9e70ffeaa49e025eae9264123e570834c867
Harsh Jain <harsh.b.jain@capgemini.com>
2025-11-09 17:02:23 +0530

    :wrench:ADD missing constants file

---
+++ user-service/src/main/java/com/app/thinknshare/user/util/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.app.thinknshare.user.util;
+
+public class ApplicationConstants {
+    public static final String JWT_SECRET_KEY = "JWT_SECRET_KEY";
+    public static final String DEFAULT_SECRET_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+    public static final String USERNAME = "username";
+    public static final String AUTHORITIES = "authorities";
+    public static final String AUTHORIZATION = "Authorization";
+}
